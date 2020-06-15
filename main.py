7e2eff9a9d7802182cc943f72a284e4c312f63b8
jefferson <jefferson2004@gmail.com>
2020-06-15 01:26:37 -0400

    Primeiro e último commit para este projeto

---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+from config import URL, URL_BASE
+import requests
+from bs4 import BeautifulSoup
+
+paginas = []
+
+# Criando um arquivo .csv
+arquivo_csv = csv.writer(open('nomes_artistas_z.csv', 'w', newline='\n'))
+arquivo_csv.writerow(['Nomes_Artistas', 'URL_Artistas'])
+
+for num_page in range(1, 5):
+    paginas.append(f"https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ{num_page}.htm")
+
+for url_por_pagina in paginas:
+    pagina = requests.get(url_por_pagina)
+    soup = BeautifulSoup(pagina.text, 'html.parser')
+
+    # Remover links inferiores
+    ultimos_links = soup.find(class_='AlphaNav')
+    ultimos_links.decompose()
+
+    # Pegar o conteúdo da body text
+    bloco_nomes_artistas = soup.find(class_='BodyText')
+    lista_nomes_artistas = bloco_nomes_artistas.find_all('a')
+
+    for nome_artista in lista_nomes_artistas:
+        nomes = nome_artista.contents[0]
+        links = f"{URL_BASE}{nome_artista.get('href')}"
+        arquivo_csv.writerow([nomes, links])
+        print(nomes)
+        print(links)
